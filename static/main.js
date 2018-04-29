a498deafe32992a34256b07f57c5e08bec8569b3
Melissa Orgill <orgillmelissa33@gmail.com>
2018-04-29 13:47:05 -0700

    adjusted map so layers for data are removed and added, normalized data for UI

--- static/main.js
+++ static/main.js
@@ -1,48 +1,125 @@
 
-  $(function(){
-    var $disasterTypeSector = $('#disasterType');
-    var $disasterYearSector = $('#disasterYear');
 
-    $('#disasterType, #disasterYear').on('change', function(e){
-      var typeSelected = $disasterTypeSector.val();
-      var yearSelected = $disasterYearSector.val();
-      
-      var url = '/api/v1/disasters';
-      if(typeSelected !== 'All' && yearSelected !== "All") {
-        // user selected both type and year.
-        url += '/yeartype/' + yearSelected + '/' + typeSelected;
-      } else if(typeSelected !== 'All') {
-        // user selected just type.
-        url += '/type/' + typeSelected;
-      } else if(yearSelected !== 'All') {
-        // user selected just year.
-        url += '/year/' + yearSelected;
+$(function(){
+  var $disasterTypeSector = $('#disasterType');
+  var $disasterYearSector = $('#disasterYear');
+
+  $('#disasterType, #disasterYear').on('change', function(e){
+    var typeSelected = $disasterTypeSector.val();
+    var yearSelected = $disasterYearSector.val();
+    
+    var url = '/api/v1/disasters';
+    if(typeSelected !== 'All' && yearSelected !== "All") {
+      // user selected both type and year.
+      url += '/yeartype/' + yearSelected + '/' + typeSelected;
+    } else if(typeSelected !== 'All') {
+      // user selected just type.
+      url += '/type/' + typeSelected;
+    } else if(yearSelected !== 'All') {
+      // user selected just year.
+      url += '/year/' + yearSelected;
+    }
+    getData(url);
+  });
+
+
+  function getData(url) {
+    // get the data from the API.
+    $.ajax({
+      method: 'GET',
+      url: url,
+      success: function(data){
+        showData(data);
+      },
+      error: function(){
+        // TODO: some UI to show that we hit an error.
+        console.error(arguments);
       }
-      getData(url);
+    });
+  }
+
+  // Update the map with the disaster information
+  function showData(data) {  
+    // TODO: Replace Object.values. Doens't work in IE
+    var highestNum = getMaxOfArray(Object.values(data));
+    
+    var normalizedData = Object.assign({}, data);
+
+    Object.keys(normalizedData).forEach(function(key) {
+      var value = normalizedData[key];
+      normalizedData[key] = (normalizedData[key] / highestNum) * 1000;
     });
 
+    
 
-    function getData(url) {
-      // get the data from the API.
-      $.ajax({
-        method: 'GET',
-        url: url,
-        success: function(data){
-          console.log(data);
-        },
-        error: function(){
-          // TODO: some UI to show that we hit an error.
-          console.error(arguments);
-        }
-      });
-    }
+    statesData.features.forEach(function(stateData){
+      var longName = stateData.properties.name;
+      var value = normalizedData[stateMapping[longName]];
+      if (value) {
+        stateData.properties.density = value;
+      } else {
+        stateData.properties.density = 0;
+      }
+    });
+    renderLayer(statesData);
 
-    // function showData(data) {
-    //   // Update the map with the disaster information
-    //   var highestValue = max([0, 1, 34, 44])
-    //   values.each(value => {
-    //     value/highestValue // 34/44 = 0.7727272727. value would be 772.
-    //   })
+  };
+});
 
-    // }
-  });
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+var stateMapping = {
+  'Alabama': 'AL',
+  'Alaska': 'AK',
+  'Arizona': 'AZ',
+  'Arkansas': 'AR',
+  'California': 'CA',
+  'Colorado': 'CO',
+  'Connecticut': 'CT',
+  'Delaware': 'DE',
+  'District Of Columbia': 'DC',
+  'Florida': 'FL',
+  'Georgia': 'GA',
+  'Hawaii': 'HI',
+  'Idaho': 'ID',
+  'Illinois': 'IL',
+  'Indiana': 'IN',
+  'Iowa': 'IA',
+  'Kansas': 'KS',
+  'Kentucky': 'KY',
+  'Louisiana': 'LA',
+  'Maine': 'ME',
+  'Maryland': 'MD',
+  'Massachusetts': 'MA',
+  'Michigan': 'MI',
+  'Minnesota': 'MN',
+  'Mississippi': 'MS',
+  'Missouri': 'MO',
+  'Montana': 'MT',
+  'Nebraska': 'NE',
+  'Nevada': 'NV',
+  'New Hampshire': 'NH',
+  'New Jersey': 'NJ',
+  'New Mexico': 'NM',
+  'New York': 'NY',
+  'North Carolina': 'NC',
+  'North Dakota': 'ND',
+  'Ohio': 'OH',
+  'Oklahoma': 'OK',
+  'Oregon': 'OR',
+  'Pennsylvania': 'PA',
+  'Rhode Island': 'RI',
+  'South Carolina': 'SC',
+  'South Dakota': 'SD',
+  'Tennessee': 'TN',
+  'Texas': 'TX',
+  'Utah': 'UT',
+  'Vermont': 'VT',
+  'Virginia': 'VA',
+  'Washington': 'WA',
+  'West Virginia': 'WV',
+  'Wisconsin': 'WI',
+  'Wyoming': 'WY'
+};
--- static/map.js
+++ static/map.js
@@ -1,39 +1,68 @@
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fwcmlwb3QiLCJhIjoiY2pjMDJqcDhsMDQ2MzJ4bW85MTR0YXBzYiJ9.Ag9mIZTDONNN9JdN2kW76g';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    maxZoom: 5.5,
-    minZoom: 1.8,
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: [-115.36957574368233, 50.732480262447524],
-    zoom: 2.850019725398168
-  });
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fwcmlwb3QiLCJhIjoiY2pjMDJqcDhsMDQ2MzJ4bW85MTR0YXBzYiJ9.Ag9mIZTDONNN9JdN2kW76g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  maxZoom: 5.5,
+  minZoom: 1.8,
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-115.36957574368233, 50.732480262447524],
+  zoom: 2.850019725398168
+});
 
-  map.on('load', function () {
+map.on('load', function () {
+  renderLayer(statesData);
+});
 
-    var layers = map.getStyle().layers;
+map.on('click', 'us-states', function(e) {
+  var coordinates = almostFlatten(e.features[0].geometry.coordinates);
+  var bounds = new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]);
+  coordinates.forEach(function(coord) {
+    bounds.extend(coord);
+  })
 
-    // Find the index of the first symbol layer in the map style
-    var firstSymbolId;
-    for (var i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol') {
-            firstSymbolId = layers[i].id;
-            break;
-        }
-    }
+  map.fitBounds(bounds, { padding: 100 });
+});
 
-    var statesLayer = map.addLayer({
-      'id': 'us-states',
+
+function almostFlatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten[0]) ? almostFlatten(toFlatten) : [toFlatten]);
+  }, []);
+}
+
+function renderLayer(data) {
+  var layers = map.getStyle().layers;
+
+  // Find the index of the first symbol layer in the map style
+  var firstSymbolId;
+  for (var i = 0; i < layers.length; i++) {
+      if (layers[i].type === 'symbol') {
+          firstSymbolId = layers[i].id;
+          break;
+      }
+  }
+
+  var stateLayerId = 'us-states';
+  if(map.getLayer(stateLayerId)) {
+    map.removeLayer(stateLayerId);
+  }
+  
+  if(map.getSource(stateLayerId)) {
+    map.removeSource(stateLayerId);
+  }
+
+  var statesLayer = map.addLayer({
+      'id': stateLayerId,
       'type': 'fill',
       'source': {
         type: 'geojson',
-        data: statesData
+        data: data
       },
       'paint': {
         'fill-color': {
           property: 'density',
           stops: [
-              [0, '#70F1FF'],
+              [0, '#808080'],
               [10, '#56D7FF'],
               [20, '#3DBEFF'],
               [50, '#23A4FF'],
@@ -46,22 +75,4 @@
         'fill-opacity': 0.75
       }
     }, firstSymbolId);
-
-  });
-
-  map.on('click', 'us-states', function(e) {
-    var coordinates = almostFlatten(e.features[0].geometry.coordinates);
-    var bounds = new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]);
-    coordinates.forEach(function(coord) {
-      bounds.extend(coord);
-    })
-
-    map.fitBounds(bounds, { padding: 100 });
-  });
-
-
-  function almostFlatten(arr) {
-    return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten[0]) ? almostFlatten(toFlatten) : [toFlatten]);
-    }, []);
-  }
+}
